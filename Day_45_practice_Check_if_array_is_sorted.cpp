17abdeee4f248def9cf62a3ed283eb41e44ae880
Vedang Maheshwari <98143931+vedang1@users.noreply.github.com>
2024-03-13 12:10:16 +0530

    Create Day_45_practice_Check_if_array_is_sorted.cpp

---
+++ Day_45_practice_Check_if_array_is_sorted.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    bool arraySortedOrNot(int arr[], int n) {
+        // code here
+        for (int i = 1; i < n; i++) {
+            if (arr[i] < arr[i - 1])
+                 return 0;
+    }
+
+    return 1;
+    }
+};
