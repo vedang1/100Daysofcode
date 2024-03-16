0b1d5b8d82583d2150fefd29eccf97ca0d9c4533
Vedang Maheshwari <98143931+vedang1@users.noreply.github.com>
2024-03-16 13:37:41 +0530

    Create Day_48_Delete_without_head_pointer.cpp

---
+++ Day_48_Delete_without_head_pointer.cpp
@@ -0,0 +1,11 @@
+class Solution
+{
+    public:
+    //Function to delete a node without any reference to head pointer.
+    void deleteNode(Node *del_node)
+    {
+        // Your code here
+        del_node->data = del_node->next->data;
+        del_node->next = del_node->next->next;
+    }
+};
