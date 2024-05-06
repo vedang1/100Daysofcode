dcc9d4b235423ff87cd5e64f52c240bd8a7cd753
Vedang Maheshwari <98143931+vedang1@users.noreply.github.com>
2024-05-06 19:16:45 +0530

    Create Day_99_Print_all_nodes_that_don't_have_sibling.cpp

---
+++ Day_99_Print_all_nodes_that_don't_have_sibling.cpp
@@ -0,0 +1,33 @@
+vector<int> noSibling(Node* node)
+{
+    // code here
+queue<Node*> q;
+   vector<int> ans;
+   if(node == NULL){
+       return ans;
+    }
+   q.push(node);
+   while(q.empty() == false){
+       Node* curr = q.front();
+       q.pop();
+       if(curr->left != NULL){
+           if(curr->right == NULL){
+               ans.push_back(curr->left->data);
+           }
+           q.push(curr->left);
+       }
+       if(curr->right != NULL){
+           if(curr->left == NULL){
+               ans.push_back(curr->right->data);
+           }
+           q.push(curr->right);
+       }
+   }
+   if(ans.size() == 0){
+       return {-1};
+   }
+   else{
+       sort(ans.begin(), ans.end());
+       return ans;
+   }
+};
